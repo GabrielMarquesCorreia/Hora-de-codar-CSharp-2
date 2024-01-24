0b5a99e8e0c9d5b0b5318c473f4f1f6618c31904
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-24 12:53:18 -0300

    Add files via upload

---
+++ Dez Valores/Dez Valores/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Soma de 10 números");
+
+float soma = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Escreva o {i}° número: ");
+    float num = float.Parse( Console.ReadLine() );
+    soma += num;
+};
+
+Console.WriteLine($"A soma de todos esses números é igual a: {soma}");
