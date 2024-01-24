d1720d98d8fef237c9dc7fc4660ed61a775ea081
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-24 12:55:49 -0300

    Add files via upload

---
+++ Notas/Notas/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine($"Boletim");
+
+Console.Write("Informe o nome do aluno: ");
+string name = Console.ReadLine();
+
+Console.Write("Escreva a nota do aluno em português: ");
+float n1 = float.Parse( Console.ReadLine() );
+
+Console.Write("Escreva a nota do aluno em matemática: ");
+float n2 = float.Parse(Console.ReadLine());
+
+Console.Write("Escreva a nota do aluno em inglês: ");
+float n3 = float.Parse(Console.ReadLine());
+
+Console.Write("Escreva a nota do aluno em filosofia: ");
+float n4 = float.Parse(Console.ReadLine());
+
+Console.Write("Escreva a nota do aluno em informática: ");
+float n5 = float.Parse(Console.ReadLine());
+
+double r = (n1 + n2 + n3 + n4 + n5) / 5;
+
+if (r >= 6)
+{
+    Console.WriteLine($"A média final do aluno {name} é igual a: {r.ToString("N1")}. O aluno foi aprovado.");
+}
+else
+{
+    Console.WriteLine($"A média final do aluno {name} é igual a: {r.ToString("N2")}. O aluno foi reprovado.");
+}
