a8bfc2460a669a953f420f3fb93c9aeced845adb
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-24 12:59:10 -0300

    Add files via upload

---
+++ Atividade 3/Atividade 3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ola, esse programa irá pegar 10 valores e informar a média desses valores");
+
+double cont = 1;
+double soma = 0;
+// double numb = 0;
+double media = 0;
+
+while (cont <= 10)
+{
+    Console.WriteLine($"Por favor informe um valor {cont}: ");
+    float numb = float.Parse(Console.ReadLine());
+    soma += numb;
+    media = (soma) / 10;
+    cont++;
+};
+
+Console.WriteLine($"A média deste valores somados é igual a: {media}");
