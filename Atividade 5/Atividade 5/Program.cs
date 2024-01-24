40328db077e37f5c4c91dfeafa88a452a76e55db
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-24 12:59:38 -0300

    Add files via upload

---
+++ Atividade 5/Atividade 5/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Aprovado / Reprovado");
+
+double soma = 0;
+double media = 0;
+
+for (int i = 1; i < 6; i++)
+{
+    Console.WriteLine($"Informe a sua {i}° nota: ");
+    float nota = float.Parse( Console.ReadLine() );
+    soma += nota;
+    media = soma / 5;
+}
+
+if (media >= 6.0)
+{
+    Console.WriteLine($"A sua média é igual a: {media.ToString("N2")}. Parabéns! Você foi aprovado!");
+}
+else if (media < 6.0)
+{
+    Console.WriteLine($"A sua média é igual a: {media.ToString("N2")}. Você foi reprovado! Parabéns! Se esforce mais da próxima vez!");
+}
