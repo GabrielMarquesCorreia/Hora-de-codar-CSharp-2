49b1d6ef81e5fc618c410ae2995f4aef53a5f60d
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-24 12:58:33 -0300

    Add files via upload

---
+++ Atividade 2/Atividade 2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Olá, esse programa irá dizer o maior número entre 3 que você escrever");
+
+float cont = 1;
+float maior = 0;
+
+while (cont <= 3)
+{
+    Console.WriteLine("Por favor informe um número: ");
+    float n1 = float.Parse(Console.ReadLine());
+
+    if (n1 > maior)
+    {
+        maior = n1;
+    }
+    else if (n1 == maior)
+    {
+        Console.WriteLine("OPA! Por favor, informe um valor diferente!");
+        cont--;
+    }
+    cont++;
+};
+
+Console.WriteLine($"O maior valor informado é... {maior}");
