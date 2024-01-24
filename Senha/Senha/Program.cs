c7dddb751169bcd7310bb79a9ff23d29b9e631df
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-24 13:00:14 -0300

    Add files via upload

---
+++ Senha/Senha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite a sua senha: ");
+
+int senha = int.Parse(Console.ReadLine());
+
+if (senha == 1234)
+{
+    Console.WriteLine("Senha correta, acesso permitido. Bem-Vindo ao servidor!");
+}
+else
+{
+    Console.WriteLine("SENHA INCORRETA!!! ACESSO NEGADO!!! CHAMANDO A POLÍCIA!!!!");
+}
